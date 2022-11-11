f3b07f9c70942a38bfc8e3ee6d25010eb6af9c4d
PERRIER <maxperrier87@gmail.com>
2022-11-11 23:24:26 +0100

    Système de commentaire

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,19 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/article')]
 class ArticleController extends AbstractController
 {
-    #[Route('/', name: 'app_article_index', methods: ['GET'])]
-    public function index(ArticleRepository $articleRepository): Response
-    {
-        return $this->render('article/index.html.twig', [
-            'articles' => $articleRepository->findAll(),
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_article_show', methods: ['GET'])]
     public function show(Article $article): Response
     {
+        $comments = $article->getComments();
         return $this->render('article/show.html.twig', [
             'article' => $article,
+            'comments' => $comments
         ]);
     }
 }
